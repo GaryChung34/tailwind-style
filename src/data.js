af93efa04de48d4947ee8e9a14b2da244243c271
Gary Chung <gary64800@gmail.com>
2021-10-17 16:52:31 +0800

    add employee testing photo.

--- src/data.js
+++ src/data.js
@@ -3,34 +3,34 @@
 		id: 'A00101',
 		name: '員工 A',
 		site: 'Site A'
-		photo: '/img/staff_1.jpg'
+		photo: '/employee_photo/staff_1.jpg'
 	},
 
 	{
 		id: 'A06866',
 		name: '員工 B',
 		site: 'Site B'
-		photo: '/img/staff_2.jpg'
+		photo: '/employee_photo/staff_2.jpg'
 	},
 
 	{
 		id: 'A06571',
 		name: '員工 C',
 		site: 'Site C'
-		photo: '/img/staff_3.jpg'
+		photo: '/employee_photo/staff_3.jpg'
 	},
 
 	{
 		id: 'A07104',
 		name: '員工 D',
 		site: 'Site D'
-		photo: '/img/staff_4.jpg'
+		photo: '/employee_photo/staff_4.jpg'
 	},
 
 	{
 		id: 'A12345',
 		name: '員工 E',
 		site: 'Site E'
-		photo: '/img/staff_5.jpg'
+		photo: '/employee_photo/staff_5.jpg'
 	}
 ]
